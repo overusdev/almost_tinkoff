2e7a68c0d0036ba1c4644ca14ad76fad21dbf023
Сергей Вилков <you@example.com>
2020-01-02 12:18:03 +0300

    Slide menu script

--- src/js/index.js
+++ src/js/index.js
@@ -3,5 +3,16 @@ import popper from "popper.js";
 import bootstrap from "bootstrap";
 
 jQuery(function() {
+    var $gamburger = $('.g_nav__top-gamburger');
+    var $slideMenu = $('.g_nav__slide-menu');
+    var $slideMenuClose = $('.g_nav__slide-menu-close-icon');
+
+    $gamburger.click(function () {
+        $slideMenu.addClass('g_nav__slide-menu_state_active');
+    });
+
+    $slideMenuClose.click(function () {
+        $slideMenu.removeClass('g_nav__slide-menu_state_active');
+    });
 
 });
